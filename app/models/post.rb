2afe8e7b991efb673f6e3801c12d3dd1730709e7
jmhar <jmhar@protonmail.com>
2021-10-14 17:41:56 +1100

    validate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
     validates :title, presence: true
-    validates :content, presence: true, length: { minimum: 250 }
+    validates :content, presence: true, length: { minimum: 100 }
     validates :category, inclusion: %w(Fiction Non-Fiction)
 end
